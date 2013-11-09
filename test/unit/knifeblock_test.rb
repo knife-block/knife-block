978e5b6fbe6a896b8492a4c31a1ff7609d877678
scottphlps <scottphlps@gmail.com>
2013-11-09 16:12:05 -0500

    Update locate_config_file scope to reflect its change to a Class method
    The locate_config_file was changed to a class method on 2013-10-09:
    https://github.com/opscode/chef/commit/2a214bcff7174b61129a7a8a642b8df5b5f4afee#diff-1220e994288a7f7d9ae83196ce7fad9eL317
    
    * Fixes scope, and add abbreviated assignment for config[:config_file]
    * Adds tests for two GreenAdnSecure Module methods
    
    Refactor TestKnifeBlockList
    
    * Removes ! in front of File.symlink?
      ** Test now fails only if knife.rb is not a symlink
    * Set @chef_path to fallback to ~/.chef if WORKSPACE not defined
      ** This could also be sourced from locate_config_file which
         checks in more locations ($KNIFE_HOME, pwd, $HOME)

--- test/unit/knifeblock_test.rb
+++ test/unit/knifeblock_test.rb
@@ -6,11 +6,32 @@ require 'chef/knife'
 require './lib/chef/knife/block'
 require 'test/unit'
 
-class TestKnifeBlock < Test::Unit::TestCase
+class TestGreenAndSecureModule < Test::Unit::TestCase
 
-    def test_list_file 
-    	chef_path = "#{ENV['WORKSPACE']}/.chef/"
-    	knifeblock = GreenAndSecure::BlockList.new
-	assert(!File.symlink?(chef_path+"/knife.rb"), "Knife.rb was not a symlink")
+  def setup
+    @@green = Object.new
+    class << @@green
+      include GreenAndSecure
     end
+  end
+
+  def test_001_it_detects_a_version
+    assert_match(/\d+\.\d+\.\d+/,@@green.current_chef_version.to_s, "FAIL: Chef version undetected!")
+  end
+
+  def test_002_it_locates_a_knife_config
+    assert_match(/.*\/.chef\/knife\.rb/,@@green.chef_config_base+"/knife.rb", "FAIL: knife.rb not found!")
+  end
+end
+
+class TestKnifeBlockList < Test::Unit::TestCase
+
+  def setup
+    @@knifeblock = GreenAndSecure::BlockList.new
+    ENV['WORKSPACE'] ? (@chef_path ||= "#{ENV['WORKSPACE']}/.chef") : @chef_path = "#{ENV['HOME']}/.chef"
+  end
+
+  def test_list_file
+	  assert(File.symlink?(@chef_path+"/knife.rb"), "knife.rb was not a symlink, please copy the file to #{@chef_path}/knife-<servername>.rb")
+  end
 end
