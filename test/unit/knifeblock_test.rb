4b48567380f6cc9c715cecc283633e732e81fcdb
scottphlps <scottphlps@gmail.com>
2013-11-12 10:11:20 -0500

    Adds additional setup to make TravisCI happy

--- test/unit/knifeblock_test.rb
+++ test/unit/knifeblock_test.rb
@@ -5,33 +5,40 @@
 require 'chef/knife'
 require './lib/chef/knife/block'
 require 'test/unit'
+require 'fileutils'
 
 class TestGreenAndSecureModule < Test::Unit::TestCase
 
   def setup
-    @@green = Object.new
-    class << @@green
+    @green = Object.new
+    class << @green
       include GreenAndSecure
+    end
+
+    ENV['WORKSPACE'] ? (@chef_path ||= "#{ENV['WORKSPACE']}/.chef") : @chef_path = "#{ENV['HOME']}/.chef"
+    if ENV['TRAVIS']
+      @knife_ci = "#{@chef_path}/knife-ci.rb"
+      FileUtils.mkpath(@chef_path) unless File.exists?(@chef_path)
+      FileUtils.touch("#{@knife_ci}") unless File.exists?("#{@knife_ci}")
+      FileUtils.ln_s("#{@knife_ci}","#{@chef_path}/knife.rb") unless File.exists?("#{@chef_path}/knife.rb")
+    end
+  end
+
+  def teardown
+    if ENV['TRAVIS']
+      FileUtils.remove_entry_secure(@chef_path, force = true) if File.exists?(@chef_path)
     end
   end
 
   def test_001_it_detects_a_version
-    assert_match(/\d+\.\d+\.\d+/,@@green.current_chef_version.to_s, "FAIL: Chef version undetected!")
+    assert_match(/\d+\.\d+\.\d+/,@green.current_chef_version.to_s, "FAIL: Chef version undetected!")
   end
 
   def test_002_it_locates_a_knife_config
-    assert_match(/.*\/.chef\/knife\.rb/,@@green.chef_config_base+"/knife.rb", "FAIL: knife.rb not found!")
-  end
-end
-
-class TestKnifeBlockList < Test::Unit::TestCase
-
-  def setup
-    @@knifeblock = GreenAndSecure::BlockList.new
-    ENV['WORKSPACE'] ? (@chef_path ||= "#{ENV['WORKSPACE']}/.chef") : @chef_path = "#{ENV['HOME']}/.chef"
+    assert_match(/.*\/.chef\/knife\.rb/,@green.chef_config_base+"/knife.rb", "FAIL: knife.rb not found!")
   end
 
-  def test_list_file
-	  assert(File.symlink?(@chef_path+"/knife.rb"), "knife.rb was not a symlink, please copy the file to #{@chef_path}/knife-<servername>.rb")
+  def test_003_knife_rb_is_a_symlink
+    assert(File.symlink?(@chef_path+"/knife.rb"), "knife.rb was not a symlink, please link it to #{@chef_path}/knife-<servername>.rb")
   end
 end
